de5344be6e9430b2e44e34a60e7ef885049e2ce4
Thang Nguyen <thangnv1029@gmail.com>
2026-01-30 13:18:23 +0700

    Connect MongoDB env variables

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,9 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import 'dotenv/config'
+
+export const env = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  LOCAL_DEV_APP_HOST: process.env.LOCAL_DEV_APP_HOST,
+  LOCAL_DEV_APP_PORT: process.env.LOCAL_DEV_APP_PORT,
+  AUTHOR: process.env.AUTHOR
+}
