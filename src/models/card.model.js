107176b1050c97fc3200677fdd33acad589f635a
Thang Nguyen <thangnv1029@gmail.com>
2026-02-03 10:27:39 +0700

    Delete column and card

--- src/models/card.model.js
+++ src/models/card.model.js
@@ -65,10 +65,20 @@ export const update = async (cardId, updateData) => {
   } catch (error) { throw new Error(error) }
 }
 
+const deleteManyByColumnId = async (columnId) => {
+  try {
+    const result = await GET_DB().collection(CARD_COLLECTION_NAME).deleteMany({
+      columnId: new ObjectId(String(columnId))
+    })
+    return result
+  } catch (error) { throw new Error(error) }
+}
+
 export const cardModel = {
   CARD_COLLECTION_NAME,
   CARD_COLLECTION_SCHEMA,
   createNew,
   fineOneById,
-  update
+  update,
+  deleteManyByColumnId
 }
